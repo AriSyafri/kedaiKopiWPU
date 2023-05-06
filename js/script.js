a85bd9ed4e58b0ae841e08adb7bbfc2a7c34dcaa
AriSyafri <diamondThink15@gmail.com>
2023-05-06 09:34:37 +0700

    menambah search

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
-// klik diluar sidbar untuk menghilangkan nav
+// klik diluar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector('#hamburger-menu');
 document.addEventListener('click',function(e){
     if(!hamburger.contains(e.target)&& !navbarNav.contains(e.target)) {
